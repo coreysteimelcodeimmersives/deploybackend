958a4025550e1622e3f3371b283c5e45b417582c
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-07-25 13:57:48 -0600

    dog

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,15 @@ router.post("/post-message", (req, res) => {
   }
 });
 
+router.get("/get-dog", async (req, res) => {
+  try {
+    const dogFetch = await fetch("https://dog.ceo/api/breeds/image/random");
+    const dogJSON = await dogFetch.json();
+    const dogURL = dogJSON.message;
+    res.json({ serverMessage: dogURL }).status(200);
+  } catch (error) {
+    res.json({ serverMessage: error }).status(400);
+  }
+});
+
 module.exports = router;
