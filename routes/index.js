d6328fb9e23be89247fb1f061b9dff7f0c9b4c40
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-07-25 14:11:06 -0600

    console logs

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,14 @@ router.post("/post-message", (req, res) => {
 router.get("/get-dog", async (req, res) => {
   try {
     const dogFetch = await fetch("https://dog.ceo/api/breeds/image/random");
+    console.log("dogfetch");
+    console.log(dogFetch);
     const dogJSON = await dogFetch.json();
+    console.log("dogJSON");
+    console.log(dogJSON);
     const dogURL = dogJSON.message;
+    console.log("dogURL");
+    console.log(dogURL);
     res.json({ serverMessage: dogURL }).status(200);
   } catch (error) {
     res.json({ serverMessage: error }).status(400);
