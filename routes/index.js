73e8d244b6c28fa39d87646ba1ad78827763f1b5
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-07-25 13:22:58 -0600

    un cors

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 var express = require("express");
 var router = express.Router();
-var cors = require("cors");
-
-var corsOptions = {
-  origin: "https://cnfdeployfrontend.herokuapp.com/",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/post-message", cors(corsOptions), (req, res) => {
+router.post("/post-message", (req, res) => {
   try {
     const clientMessage = req.body.clientMessage;
     const dateTime = new Date();
